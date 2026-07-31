168c0d2251367820a5bf188c70e7492d617cdcce
Nico Botha <nicolaasb@pm.me>
2026-07-31 21:04:34 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, lazyPlugins } from "vite-plus";
+import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
+
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: {
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+  },
+  plugins: lazyPlugins(() => [react(), cloudflare()]),
+});
